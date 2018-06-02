3ad89983b70c165d9ea31d7b60fc190d3bfaddad
stgehans <georg.hans@student.hs-anhalt.de>
2018-06-02 23:07:50 +0200

    1

---
+++ MainMenuController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour {
+
+	public Text hS;
+
+
+	void Start () {
+		HSFunction ();
+	}
+
+    void Update () {
+		
+	}
+
+	public void Play(){
+		SceneManager.LoadScene (1);
+	}
+
+	void HSFunction(){
+		hS.text = PlayerPrefs.GetInt ("HighScore").ToString();
+	}
+}
---
+++ Snake.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Snake : MonoBehaviour {
+
+	private Snake next;
+	static public Action<String> hit;
+	public int dir, inputDir;
+	public bool isEdge = false;
+
+
+	public void SetNext (Snake IN){
+		next = IN;
+	}
+
+	public Snake GetNext (){
+		return next;
+	}
+
+	public void RemoveTail(){
+		Destroy (this.gameObject);
+	}
+
+    //handle collision
+	void OnTriggerEnter(Collider other){
+		if (hit != null)
+			hit (other.tag);
+		if (other.tag == "Food") {
+			Destroy (other.gameObject);
+		}
+	}
+
+	public void setDir(int value){
+		dir = value;
+	}
+
+	public void setInputDir(int value){
+		inputDir = value;
+		if (dir != inputDir)
+			isEdge = true;
+	}
+
+	public bool IsEdge(){
+		return isEdge;
+	}
+
+	public int getDir(){
+		return dir;
+	}
+
+	public int getInputDir(){
+		return inputDir;
+	}
+
+}
